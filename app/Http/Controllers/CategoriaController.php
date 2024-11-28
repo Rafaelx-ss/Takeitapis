583daa3d0b20fefafa1cdc4e4d8960bc3131f862
RafaelxNotebook <solisrafael895@gmail.com>
2024-11-28 15:14:00 -0600

    Controllers

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Categoria;
-use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\DireccionUsuarioController;
 
 Route::get('/getcategorias', [CategoriaController::class, 'index']);
 
+Route::get('/getdireccionesusuario',[DireccionUsuarioController::class, 'index']);
 
 Route::post('/register', [AuthController::class, 'register']);
+
