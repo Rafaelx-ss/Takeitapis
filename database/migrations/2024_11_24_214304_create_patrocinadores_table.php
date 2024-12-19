ced8649a232b18c765cf950fa10a14e486249248
Ariel <108147166+Arielo16@users.noreply.github.com>
2024-12-18 22:27:14 -0600

    Update 2024_11_24_214304_create_patrocinadores_table.php

--- database/migrations/2024_11_24_214304_create_patrocinadores_table.php
+++ database/migrations/2024_11_24_214304_create_patrocinadores_table.php
@@ -11,6 +11,7 @@ class CreatePatrocinadoresTable extends Migration
     {
         Schema::create('patrocinadores', function (Blueprint $table) {
             $table->bigIncrements('patrocinadorID');
+            $table->unsignedBigInteger('usuarioID');
             $table->binary('fotoPatrocinador')->nullable();
             $table->string('nombrePatrocinador', 255);
             $table->string('representantePatrocinador', 255);
@@ -21,13 +22,15 @@ class CreatePatrocinadoresTable extends Migration
             $table->boolean('activoPatrocinador')->default(true);
             $table->boolean('estadoPatrocinador')->default(true);
             $table->timestamps();
+
+            $table->foreign('usuarioID')->references('usuarioID')->on('usuarios')->onUpdate('restrict')->onDelete('cascade');
         });
 
         DB::table('patrocinadores')->insert([
-            ['fotoPatrocinador' => null, 'nombrePatrocinador' => 'Coca Cola', 'representantePatrocinador' => 'Juan Pérez', 'rfcPatrocinador' => 'COC-123456-789', 'correoPatrocinador' => 'coca@gmail.com', 'telefonoPatrocinador' => '1234567890', 'numeroRepresentantePatrocinador' => '1234567890', 'activoPatrocinador' => true, 'estadoPatrocinador' => true, 'created_at' => now(), 'updated_at' => now()],
-            ['fotoPatrocinador' => null, 'nombrePatrocinador' => 'Pepsi', 'representantePatrocinador' => 'Pedro Pérez', 'rfcPatrocinador' => 'PEP-123456-789', 'correoPatrocinador' => 'pepsi@gmail.com', 'telefonoPatrocinador' => '1234567890', 'numeroRepresentantePatrocinador' => '1234567890', 'activoPatrocinador' => true, 'estadoPatrocinador' => true, 'created_at' => now(), 'updated_at' => now()],
-            ['fotoPatrocinador' => null, 'nombrePatrocinador' => 'Bimbo', 'representantePatrocinador' => 'José Pérez', 'rfcPatrocinador' => 'BIM-123456-789', 'correoPatrocinador' => 'bimbo@gmail.com', 'telefonoPatrocinador' => '1234567890', 'numeroRepresentantePatrocinador' => '1234567890', 'activoPatrocinador' => true, 'estadoPatrocinador' => true, 'created_at' => now(), 'updated_at' => now()],
-            ['fotoPatrocinador' => null, 'nombrePatrocinador' => 'Sabritas', 'representantePatrocinador' => 'Jorge Pérez', 'rfcPatrocinador' => 'SAB-123456-789', 'correoPatrocinador' => 'sabritas@gmail.com', 'telefonoPatrocinador' => '1234567890', 'numeroRepresentantePatrocinador' => '1234567890', 'activoPatrocinador' => true, 'estadoPatrocinador' => true, 'created_at' => now(), 'updated_at' => now()],
+            ['usuarioID' => 1,'fotoPatrocinador' => null, 'nombrePatrocinador' => 'Coca Cola', 'representantePatrocinador' => 'Juan Pérez', 'rfcPatrocinador' => 'COC-123456-789', 'correoPatrocinador' => 'coca@gmail.com', 'telefonoPatrocinador' => '1234567890', 'numeroRepresentantePatrocinador' => '1234567890', 'activoPatrocinador' => true, 'estadoPatrocinador' => true, 'created_at' => now(), 'updated_at' => now()],
+            ['usuarioID' => 1,'fotoPatrocinador' => null, 'nombrePatrocinador' => 'Pepsi', 'representantePatrocinador' => 'Pedro Pérez', 'rfcPatrocinador' => 'PEP-123456-789', 'correoPatrocinador' => 'pepsi@gmail.com', 'telefonoPatrocinador' => '1234567890', 'numeroRepresentantePatrocinador' => '1234567890', 'activoPatrocinador' => true, 'estadoPatrocinador' => true, 'created_at' => now(), 'updated_at' => now()],
+            ['usuarioID' => 1,'fotoPatrocinador' => null, 'nombrePatrocinador' => 'Bimbo', 'representantePatrocinador' => 'José Pérez', 'rfcPatrocinador' => 'BIM-123456-789', 'correoPatrocinador' => 'bimbo@gmail.com', 'telefonoPatrocinador' => '1234567890', 'numeroRepresentantePatrocinador' => '1234567890', 'activoPatrocinador' => true, 'estadoPatrocinador' => true, 'created_at' => now(), 'updated_at' => now()],
+            ['usuarioID' => 1,'fotoPatrocinador' => null, 'nombrePatrocinador' => 'Sabritas', 'representantePatrocinador' => 'Jorge Pérez', 'rfcPatrocinador' => 'SAB-123456-789', 'correoPatrocinador' => 'sabritas@gmail.com', 'telefonoPatrocinador' => '1234567890', 'numeroRepresentantePatrocinador' => '1234567890', 'activoPatrocinador' => true, 'estadoPatrocinador' => true, 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 
